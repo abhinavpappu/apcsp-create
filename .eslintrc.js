fed85bc9e0a196b7fc5367ce5ac59394dfafe355
Abhinav Pappu <abhi1pappu9@gmail.com>
2018-02-17 12:34:54 -0600

    Set up workspace

---
+++ .eslintrc.js
@@ -0,0 +1,24 @@
+module.exports = {
+  "extends": ["airbnb-base", "plugin:vue/strongly-recommended"],
+  "parser": "vue-eslint-parser",
+  "parserOptions": {
+      "parser": "babel-eslint",
+      "sourceType": "module",
+      "ecmaVersion": 2017
+  },
+  "rules": {
+      "linebreak-style": ["error", "windows"],
+      "max-len": ["error", {
+          "ignoreComments": true,
+          "code": 120,
+          "ignorePattern": "^\\s*box-shadow"
+      }],
+      "arrow-parens": ["error", "as-needed"],
+      "no-multi-assign": "off",
+      "no-new": "off",
+      "no-plusplus": "off",
+      "vue/max-attributes-per-line": ["error", {"singleline": 4}],
+      "no-bitwise": "off",
+      "vue/require-v-for-key": "off",
+  }
+};
